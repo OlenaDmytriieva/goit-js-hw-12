590607c85afe7a0ec4c7196d6c00202f4953ab7f
OlenaDmytriieva <lela.dmitrieva@gmail.com>
2024-02-24 22:59:51 +0200

    Just another implementation

--- src/main.js
+++ src/main.js
@@ -7,112 +7,98 @@ import { loadMoreBtn } from './js/render-functions';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
+function showToast(message, type = 'warning') {
+  switch (type) {
+    case 'warning':
+      iziToast.warning({
+        title: 'Caution',
+        titleColor: '#FFF',
+        messageColor: '#FFF',
+        message: message,
+        backgroundColor: '#FFA000',
+        position: 'topRight',
+        theme: 'dark',
+        timeout: 5000,
+      });
+      break;
+    case 'error':
+      iziToast.error({
+        title: 'Error',
+        titleColor: '#FFF',
+        messageColor: '#FFF',
+        message: message,
+        backgroundColor: '#EF4040',
+        position: 'topRight',
+        theme: 'dark',
+        timeout: 5000,
+      });
+      break;
+    case 'info':
+      iziToast.info({
+        title: 'Info',
+        titleColor: '#FFF',
+        messageColor: '#FFF',
+        message: message,
+        backgroundColor: '#0099FF',
+        position: 'topRight',
+        theme: 'dark',
+        timeout: 5000,
+      });
+  }
+}
+
 const form = document.querySelector('.search-form');
 const loaderElement = document.querySelector('.loader');
-
-let page = 1;
-let limit = 15;
-let totalPages;
+let page = 0;
 let currentQuery = '';
+const limit = 15;
 
-form.addEventListener('submit', async e => {
+async function handleQuery(e) {
   e.preventDefault();
-  const query = form.querySelector('input').value.trim();
-
-  if (query) {
-    loaderElement.style.display = 'block';
-    if (currentQuery !== query) {
-      page = 1;
-    }
-    currentQuery = query;
-
-    try {
-      const data = await queryPixabay(query, page, limit);
-      const images = data.hits;
-      totalPages = Math.ceil(data.totalHits / limit);
-      if (images.length === 0) {
-        iziToast.error({
-          title: 'Error',
-          titleColor: '#FFF',
-          messageColor: '#FFF',
-          message:
-            'Sorry, there are no images matching your search query. Please try again!',
-          backgroundColor: '#EF4040',
-          position: 'topRight',
-          theme: 'dark',
-          timeout: 5000,
-        });
-      } else {
-        createGallery(images, false);
-        loaderElement.style.display = 'none';
-        if (page === totalPages) {
-          hideLoadMoreButton();
-          iziToast.info({
-            title: 'Info',
-            titleColor: '#FFF',
-            messageColor: '#FFF',
-            message:
-              "We're sorry, but you've reached the end of search results.",
-            position: 'topRight',
-            backgroundColor: '#0099FF',
-            theme: 'dark',
-            timeout: 5000,
-          });
-        } else {
-          showLoadMoreButton();
-        }
-      }
-    } catch (error) {
-      console.error('Error:', error);
-      loaderElement.style.display = 'none';
-    }
-  } else {
-    iziToast.warning({
-      title: 'Caution',
-      titleColor: '#FFF',
-      messageColor: '#FFF',
-      message: 'This input field cannot be empty. Please enter your request!',
-      backgroundColor: '#FFA000',
-      position: 'topRight',
-      theme: 'dark',
-      timeout: 5000,
-    });
-    loaderElement.style.display = 'none';
-  }
-});
-
-loadMoreBtn.addEventListener('click', async () => {
-  loaderElement.style.display = 'block';
-  hideLoadMoreButton();
   page++;
 
-  try {
-    const data = await queryPixabay(currentQuery, page, limit);
-    const images = data.hits;
-
-    if (images.length > 0) {
-      createGallery(images, true);
-      showLoadMoreButton();
-
-      if (page === totalPages) {
-        hideLoadMoreButton();
-        iziToast.info({
-          title: 'Info',
-          titleColor: '#FFF',
-          messageColor: '#FFF',
-          message: "We're sorry, but you've reached the end of search results.",
-          position: 'topRight',
-          backgroundColor: '#0099FF',
-          theme: 'dark',
-          timeout: 5000,
-        });
-      }
-    } else {
-      hideLoadMoreButton();
-    }
-  } catch (error) {
-    console.error('Error:', error);
-  } finally {
-    loaderElement.style.display = 'none';
+  const query = form.querySelector('input').value.trim();
+  // check query
+  if (!query) {
+    showToast(
+      'This input field cannot be empty. Please enter your request!',
+      'warning'
+    );
+    return;
   }
-});
+
+  // new query?
+  if (query != currentQuery) {
+    page = 1;
+  }
+  currentQuery = query;
+
+  loaderElement.style.display = 'block';
+  hideLoadMoreButton();
+  const data = await queryPixabay(query, page, limit);
+  loaderElement.style.display = 'none';
+
+  if (data.hits.length == 0) {
+    showToast(
+      'Sorry, there are no images matching your search query. Please try again!',
+      'error'
+    );
+    return;
+  }
+
+  createGallery(data.hits, page > 1);
+  const totalPages = Math.ceil(data.totalHits / limit);
+
+  if (totalPages === page) {
+    showToast(
+      "We're sorry, but you've reached the end of search results.",
+      'info'
+    );
+  } else {
+    showLoadMoreButton();
+  }
+}
+
+form.addEventListener('submit', handleQuery);
+
+loadMoreBtn.addEventListener('click', handleQuery);
